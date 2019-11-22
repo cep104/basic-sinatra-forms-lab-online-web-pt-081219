af15870be7ad04234899631f27bd238066725026
cep104 <candicelizabethpeters@gmail.com>
2019-11-22 11:23:38 -0600

    done
    finished

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do 
+        puts params
+        erb :newteam
+    end
 
-
+    post '/team' do 
+        @name = params["name"]
+        @team_coach = params["coach"]
+        @team_point = params["pg"]
+        @team_shoot = params["sg"]
+        @team_small = params["sf"]
+        @team_power = params["pf"]
+        @team_center = params["c"]
+        erb :team
+    end
 end
+
